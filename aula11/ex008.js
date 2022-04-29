9ed2ce5ed9b8bc76c168df8c5cee4d252b023838
Romildo de Campos <camposrsp@gmail.com>
2022-04-29 18:32:05 -0300

    Initial commit

---
+++ aula11/ex008.js
@@ -0,0 +1,12 @@
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel} Km/h`)//condição simple
+if(vel > 60){
+ console.log('Você ultrapassou a velocidade permitida.MULTADO!') 
+}
+
+console.log('Sempre dirija usando o cinto')
+
+
+
+
+
